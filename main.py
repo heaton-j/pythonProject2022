0c225682fcc5aaad313b25669f5215f92c2c3dda
heatonj <heatonj@stu.otc.school.nz>
2022-08-09 10:55:50 +1200

    Made the font sizes different, did more research into how to link pages to other pages on youtube

--- main.py
+++ main.py
@@ -3,22 +3,18 @@ from tkinter import *
 root = Tk()
 root.title("Physical Activity Planner and Tracker")
 
-message_text = StringVar()
-message_text.set("Welcome, please select one of the three options below")
+label1 = Label(root, text="Welcome, please select one of the three options below", height=5, font=("arial", 20))
+label1.pack()
 
-Sign_button = Button(root, text="Sign Up", width=90, height=10, bg='lightblue')
+Sign_button = Button(root, text="Sign Up", width=190, height=3, bg='lightblue', font=("arial", 40))
 Sign_button.pack()
 
-Log_button = Button(root, text="Log in", width=90, height=10, bg='pink')
+Log_button = Button(root, text="Log in", width=190, height=3, bg='pink', font=("arial", 40))
 Log_button.pack()
 
-Help_button = Button(root, text="Help", width=90, height=10, bg='lightgreen')
+Help_button = Button(root, text="Help", width=190, height=3, bg='lightgreen', font=("arial", 40))
 Help_button.pack()
 
 root.mainloop()
 
-def help():
-    print("Help")
-    Back_button = Button(root, text="Back")
-    Back_button.pack()
 
