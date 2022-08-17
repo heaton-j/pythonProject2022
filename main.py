464d200a81f4ec1548da9fae885c07faa6d1a827
heatonj <heatonj@stu.otc.school.nz>
2022-08-17 15:10:54 +1200

    Found a website which used classes, so I exprimented with using classes by going back to code avengers, have kept the original code there incase it doesnt work out!

--- main.py
+++ main.py
@@ -29,6 +29,68 @@ sign_pagetext = tkinter.Label(root, text="Please create a password between 3-6 c
 log_pagetext = tkinter.Label(root, text="Please enter your password below")
 help_pagetext = tkinter.Label(root, text="Please read below for help")
 
+
+class StartPage():
+    def __init__(self, parent, controller):
+
+        # label of frame Layout 2
+        label = tkinter.Label(self, text="Welcome, please select one of the three options below", font = 'arial')
+
+        button1 = tkinter.Button(self, text="Sign in",
+        command=lambda: controller.show_frame(Page1))
+
+        ## button to show frame 2 with text layout2
+        button2 = tkinter.Button(self, text="Log in",
+        command=lambda: controller.show_frame(Page2))
+
+        button3 = tkinter.Button(self, text="Help"),
+
+# second window frame page1
+class Page1():
+
+    def __init__(self, parent, controller):
+
+        Tk.Frame.__init__(self, parent)
+        label = tkinter.Label(self, text="Page 1", font = 'arial')
+
+        # button to show frame 2 with text
+        # layout2
+        button1 = tkinter.Button(self, text="StartPage",
+                            command=lambda: controller.show_frame(StartPage))
+
+        # button to show frame 2 with text
+        # layout2
+        button2 = tkinter.Button(self, text="Page 2",
+                            command=lambda: controller.show_frame(Page2))
+
+# third window frame page2
+class Page2():
+
+    def __init__(self, parent, controller):
+
+        Tk.Frame.__init__(self, parent)
+        label = tkinter.Label(self, text="Page 2", font='arial')
+
+        # button to show frame 2 with text
+        # layout2
+        button1 = tkinter.Button(self, text="Page 1",
+                            command=lambda: controller.show_frame(Page1))
+
+        # button to show frame 3 with text
+        # layout3
+        button2 = tkinter.Button(self, text="Startpage",
+                            command=lambda: controller.show_frame(StartPage))
+
+class Page3():
+    def __init__(self, parent, controller):
+        tkinter.Frame.__init__(self, parent)
+        label = tkinter.Label(self, text="Help", font='arial')
+
+        button1 = tkinter.Button(self, text="Log in")
+
+
+        button2 = tkinter.Button(self, text="Sign in")
+
 root.mainloop()
 
 
