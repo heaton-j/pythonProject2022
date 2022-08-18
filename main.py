1082cac4ded7841d575a4215281b8c73587e0d5c
heatonj <heatonj@stu.otc.school.nz>
2022-08-19 09:31:14 +1200

    Restared my code with a new class, put the old code into another branch incase this does not work, still need to fix a few errors in the code before I can see if this will work. I am workign with a tutorial which has worked for them in the end. (using tkinter to move between files)

--- main.py
+++ main.py
@@ -1,95 +1,36 @@
-from tkinter import *
-import tkinter
+from tkinter import *import tkinter as tk
 
 root = Tk()
 root.title("Physical Activity Planner and Tracker")
 
-label1 = Label(root, text="Welcome, please select one of the three options below", height=5, font=("arial", 20))
-label1.pack()
+class ActivityApp(tk.Tk):
+    def __init__(self):
+        Tk.Frame.__init__(self)
+        self._frame = None
+        self.switch_frame(HomePage)
 
-def sign_page():
-    sign_pagetext.pack()
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
 
-def log_page():
-    log_pagetext.pack()
-
-def help_page():
-    help_pagetext.pack()
-
-Sign_button = tkinter.Button(root, text="Sign Up", width=15, height=1, bg='lightblue', font=("arial", 30), command=sign_page)
-Sign_button.pack()
-
-Log_button = tkinter.Button(root, text="Log In", width=15, height=1, bg='pink', font=("arial", 30), command=log_page)
-Log_button.pack()
-
-Help_button = tkinter.Button(root, text="Help", width=15, height=1, bg='lightgreen', font=("arial", 30), command=help_page)
-Help_button.pack()
-
-sign_pagetext = tkinter.Label(root, text="Please create a password between 3-6 characters")
-log_pagetext = tkinter.Label(root, text="Please enter your password below")
-help_pagetext = tkinter.Label(root, text="Please read below for help")
+class HomePage(Tk.frame):
+    def __init__(self,master):
+     Tk.Frame.__init__(root,master)
+    Label1 = Label(root, text="Please select one of the options below")
+    Calendar_Button = Tk.Button(root, text="Go to Calendar",
+                                    command=lambda: master.switch_frame(CalendarPage)).pack()
 
 
-class StartPage():
-    def __init__(self, parent, controller):
+class CalendarPage(Tk.frame):
+    def __init__(self,master):
+        Tk.Frame.__init__(root,master)
+    Label2 = Label(root, text="This is the calendar page")
+    Back_Button = Tk.Button(root, text="Homepage",
+                                 command=lambda: master.switch_frame(HomePage)).pack()
 
-        # label of frame Layout 2
-        label = tkinter.Label(self, text="Welcome, please select one of the three options below", font = 'arial')
-
-        button1 = tkinter.Button(self, text="Sign in",
-        command=lambda: controller.show_frame(Page1))
-
-        ## button to show frame 2 with text layout2
-        button2 = tkinter.Button(self, text="Log in",
-        command=lambda: controller.show_frame(Page2))
-
-        button3 = tkinter.Button(self, text="Help"),
-
-# second window frame page1
-class Page1():
-
-    def __init__(self, parent, controller):
-
-        Tk.Frame.__init__(self, parent)
-        label = tkinter.Label(self, text="Page 1", font = 'arial')
-
-        # button to show frame 2 with text
-        # layout2
-        button1 = tkinter.Button(self, text="StartPage",
-                            command=lambda: controller.show_frame(StartPage))
-
-        # button to show frame 2 with text
-        # layout2
-        button2 = tkinter.Button(self, text="Page 2",
-                            command=lambda: controller.show_frame(Page2))
-
-# third window frame page2
-class Page2():
-
-    def __init__(self, parent, controller):
-
-        Tk.Frame.__init__(self, parent)
-        label = tkinter.Label(self, text="Page 2", font='arial')
-
-        # button to show frame 2 with text
-        # layout2
-        button1 = tkinter.Button(self, text="Page 1",
-                            command=lambda: controller.show_frame(Page1))
-
-        # button to show frame 3 with text
-        # layout3
-        button2 = tkinter.Button(self, text="Startpage",
-                            command=lambda: controller.show_frame(StartPage))
-
-class Page3():
-    def __init__(self, parent, controller):
-        tkinter.Frame.__init__(self, parent)
-        label = tkinter.Label(self, text="Help", font='arial')
-
-        button1 = tkinter.Button(self, text="Log in")
-
-
-        button2 = tkinter.Button(self, text="Sign in")
 
 root.mainloop()
 
