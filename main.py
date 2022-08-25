b2888259c4f038447021a1aeaf2c8b223f442948
heatonj <heatonj@stu.otc.school.nz>
2022-08-26 09:34:03 +1200

    Did research on both how to add a calendar, managd to add a calendar at one point but wasnt going onto the gui, did reasearch on how to add a tracker, found some sites on reddit but couldnt access because it was blocked by the school. I changed the height of the home button on the pages to a smaller height, tried to make everything alighned to the center. I did research on how to make the font bold but didnt work.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from tkinter import *
+import calendar
 
 def raise_frame(frame):
     frame.tkraise()
 
 root = Tk()
 root.title("Physical activity planner and tracker")
+root.geometry("775x600")
 
 Home = Frame(root)
 Calendar = Frame(root)
@@ -25,17 +27,17 @@ Button(Home, text='Log Out', width=90, height=5, font=50, bg='red', command=root
 # creating the labels and buttons that will be shown on the calendar page
 Label(Calendar, text='Calendar', width=90, height=5, font=80).pack()
 Label(Calendar, text='Welcome to the Calendar Page', width=50, height=3, font=50).pack()
-Button(Calendar, text='Home', width=50, height=5, font=50, bg='lightyellow', command=lambda: raise_frame(Home)).pack()
+Button(Calendar, text='Home', width=30, height=2, font=50, bg='lightyellow', command=lambda: raise_frame(Home)).pack()
 
 # creating the labels and buttons that will be shown on the tracker page
 Label(Tracker, text='Tracker', width=90, height=5, font=80).pack()
 Label(Tracker, text='Welcome to the Tracker Page', width=50, height=3, font=50).pack()
-Button(Tracker, text='Home', width=50, height=5, font=50, bg='lightyellow', command=lambda: raise_frame(Home)).pack()
+Button(Tracker, text='Home', width=30, height=2, font=50, bg='lightyellow', command=lambda: raise_frame(Home)).pack()
 
 # creating the labels and buttons that will be shown on the help page
 Label(Help, text='Help', width=90, height=5, font=80).pack()
 Label(Help, text='Welcome to the Help Page', width=50, height=3, font=50).pack()
-Button(Help, text='Home', width=50, height=5, font=50, bg='lightyellow', command=lambda: raise_frame(Home)).pack()
+Button(Help, text='Home', width=30, height=2, font=50, bg='lightyellow', command=lambda: raise_frame(Home)).pack()
 
 raise_frame(Home)
 
