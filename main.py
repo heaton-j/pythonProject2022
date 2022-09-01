1f23289e72b803b57ee880315d30be4636e2f15d
heatonj <heatonj@stu.otc.school.nz>
2022-09-02 09:33:17 +1200

    Changed the enter hours box for the user to enter their hours into a combobox with different options for hours and minutes to prevent unwanted values such as words being entered. Need to figure out how to print these values onto the page, will need to create a function to do this.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def raise_frame(frame):
     frame.tkraise()
 
 root = Tk()
-root.title("Physical activity planner and tracker")
+root.title("Physical Activity Planner and Tracker")
 root.geometry("775x600")
 
 Home = Frame(root)
@@ -35,19 +35,30 @@ Label(Tracker, text='Tracker', width=90, height=3, font=80).pack()
 Label(Tracker, text='Welcome to the Tracker Page', width=50, height=1, font=50).pack()
 Label(Tracker, text='Click on the box below to enter hours for each day where physical activity was completed :', bg='lightpink').pack()
 
-Label(Tracker, text='The amount of physical activity done for the past week :', font=50, height=5).pack()
+Label(Tracker, text='The amount of physical activity done for the past week :', font=50, height=3).pack()
 
-Label(Tracker, text='Day :', width=25, height=3, font=20).pack(pady=2, padx=3)
+Label(Tracker, text='Day :', width=25, height=2, font=20).pack(pady=2, padx=3)
 day_choice = StringVar()
 Day_frame = ttk.Combobox(Tracker, textvariable=day_choice, width=25, height=8)
 Day_frame['state'] = 'readonly'
 Day_frame['values'] = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
 Day_frame.pack(pady=2, padx=3)
 
+Label(Tracker, text='Hours and Minutes done :', width=25, height=2, font=20, ).pack(pady=2, padx=3)
+hour_choice = StringVar()
+Hour_frame = ttk.Combobox(Tracker, textvariable=hour_choice, width=25, height=8)
+Hour_frame['state'] = 'readonly'
+Hour_frame['values'] = ('0 hr', '1 hr', '2 hr', '3 hr', '4 hr', '5 hr')
+Hour_frame.pack(pady=2, padx=3)
+minute_choice = StringVar()
+Minute_frame = ttk.Combobox(Tracker, textvariable=minute_choice, width=25, height=8)
+Minute_frame['state'] = 'readonly'
+Minute_frame['values'] = ('5 minutes', '10 minutes', '15 minutes', '20 minutes', '25 minutes', '30 minutes', '35 minutes',
+                          '40 minutes', '45 minutes', '50 minutes', '55 minutes')
+Minute_frame.pack(pady=20, padx=3)
 
-Label(Tracker, text='Hours done :', width=25, height=3, font=20).pack(pady=2, padx=3)
-Hours_frame = Entry(Tracker, width=15, font=('Arial', 24), bg='grey')
-Hours_frame.pack(pady=2, padx=3)
+# button for user to enter their data chosen
+Button(Tracker, text='Enter', width=10, height=3).pack(pady=5)
 
 # button that allows users to go back to the home page
 Button(Tracker, text='Home', width=30, height=2, font=50, bg='lightyellow', command=lambda: raise_frame(Home)).pack()
