8c0ecf0610fb4ae11b8e87c12bdad641a91e0558
heatonj <heatonj@stu.otc.school.nz>
2022-09-07 15:14:02 +1200

    Made main text at the top of the page bolder for the user, tried to figure out why my .show has no attribute when I tried to print what the user has chosen from the options in the comboboxes.

--- main.py
+++ main.py
@@ -6,38 +6,31 @@ def raise_frame(frame):
 
 root = Tk()
 root.title("Physical Activity Planner and Tracker")
-root.geometry("775x600")
+root.geometry("800x575")
 
 Home = Frame(root)
-Calendar = Frame(root)
 Tracker = Frame(root)
 Help = Frame(root)
 Day_entry = Frame(root)
 
-for frame in (Home, Calendar, Tracker, Help):
+for frame in (Home, Tracker, Help):
     frame.grid(row=100, column=100, sticky='NEWS')
 
 # creating the labels and buttons that will be shown on the home page
-Label(Home, text='Home', width=90, height=5, font=190).pack()
-Label(Home, text='Please select one of the options below to continue', width=90, height=5, font=100).pack()
-home_calendar = Button(Home, text='Calendar', width=90, height=5, font=50, bg='lightblue', command=lambda: raise_frame(Calendar)).pack()
-home_tracker= Button(Home, text='Tracker', width=90, height=5, font=50, bg='lightgreen', command=lambda: raise_frame(Tracker)).pack()
-home_help = Button(Home, text='Help', width=90, height=5, font=50, bg='lightpink', command=lambda: raise_frame(Help)).pack()
-Button(Home, text='Log Out', width=90, height=5, font=50, bg='red', command=root.destroy).pack()
+Label(Home, text='Home', width=8, height=4, font='sans 16 bold').pack()
 
-# creating the labels and buttons that will be shown on the calendar page
-Label(Calendar, text='Calendar', width=90, height=5, font=80).pack()
-Label(Calendar, text='Welcome to the Calendar Page', width=50, height=3, font=50).pack()
-Button(Calendar, text='Home', width=30, height=2, font=50, bg='lightyellow', command=lambda: raise_frame(Home)).pack()
+Label(Home, text='Please select one of the options below to continue', width=90, height=6, font=100).pack()
+home_tracker= Button(Home, text='Tracker', width=90, height=6, font=50, bg='lightgreen', command=lambda: raise_frame(Tracker)).pack()
+home_help = Button(Home, text='Help', width=90, height=6, font=50, bg='lightpink', command=lambda: raise_frame(Help)).pack()
+Button(Home, text='Log Out', width=90, height=6, font=50, bg='red', command=root.destroy).pack()
 
 # creating the labels and buttons that will be shown on the tracker page
-Label(Tracker, text='Tracker', width=90, height=3, font=80).pack()
-Label(Tracker, text='Welcome to the Tracker Page', width=50, height=1, font=50).pack()
+Label(Tracker, text='Tracker', width=90, height=3, font='sans 11 bold').pack()
 Label(Tracker, text='Click on the box below to enter hours for each day where physical activity was completed :', bg='lightpink').pack()
 
 Label(Tracker, text='The amount of physical activity done for the past week :', font=50, height=3).pack()
 
-# Label and combobox where users select the day where they want to enter their data
+# Label and combobox where users select the day they want to enter their data
 Label(Tracker, text='Day :', width=25, height=2, font=20).pack(pady=2, padx=3)
 day_choice = StringVar()
 Day_frame = ttk.Combobox(Tracker, textvariable=day_choice, width=25, height=8)
@@ -65,22 +58,21 @@ Minute_frame.pack(pady=20, padx=3)
 
 def getSelection(Day_frame, Hour_frame, Minute_frame):
     val = Day_frame.get(), Hour_frame.get(), Minute_frame.get()
-    print.showinfo("Selection", val)
+    print.show("Selection", val)
 
 
 # Button for user to enter their data chosen
 Enter_button = ttk.Button(Tracker, text='Enter', width=10,
-                command= lambda: getSelection(Day_frame, Hour_frame, Minute_frame)).pack(pady=5)
+                          command= lambda: getSelection(Day_frame, Hour_frame, Minute_frame)).pack(pady=5)
 
 # button that allows users to go back to the home page
-Button(Tracker, text='Home', width=30, height=2, font=50, bg='lightyellow', command=lambda: raise_frame(Home)).pack(
-    ipadx=10, ipady=10, expand=True, side='left'
-)
+Button(Tracker, text='Home', width=15, height=1, font=50, bg='lightblue', command=lambda: raise_frame(Home)).pack(
+    ipadx=10, ipady=10, expand=True, side='left')
 
 # creating the labels and buttons that will be shown on the help page
 Label(Help, text='Help', width=90, height=5, font=80).pack()
 Label(Help, text='Welcome to the Help Page', width=50, height=3, font=50).pack()
-Button(Help, text='Home', width=30, height=2, font=50, bg='lightyellow', command=lambda: raise_frame(Home)).pack()
+Button(Help, text='Home', width=30, height=2, font=50, bg='lightblue', command=lambda: raise_frame(Home)).pack()
 
 raise_frame(Home)
 
