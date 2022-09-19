a7e07c3652b78b0a2a55685c433c00af4ea09557
heatonj <heatonj@stu.otc.school.nz>
2022-09-19 13:24:51 +1200

    Made a window pop-up thing that pops up when the user clicks the enter button on the tracker page. Need to format this correctly or find another way to present the values that the user has entered. Changed both the comboboxes for the hours, minutes, and day to entry boxes to meet the requriement of 2 types of input as data. Need to find a way to validate these entrys so there are only certain values that the user can enter. Also did research on how to add pictures into the help page, didnt work so need to do more research on this aspect.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+
 
 def raise_frame(frame):
     frame.tkraise()
@@ -26,44 +28,46 @@ Button(Home, text='Log Out', width=90, height=6, font=50, bg='red', command=root
 
 # creating the labels and buttons that will be shown on the tracker page
 Label(Tracker, text='Tracker', width=90, height=3, font='sans 11 bold').pack()
+
 Label(Tracker, text='Click on the box below to enter hours for each day where physical activity was completed :', bg='lightpink').pack()
 
-Label(Tracker, text='The amount of physical activity done for the past week :', font=50, height=3).pack()
+Label(Tracker, text='!!!Remember at least 30 minutes of moderate physical activty is recommended each day!!!').pack()
+
+Label(Tracker, text='The amount of physical activity done for this day :', font=50, height=3).pack()
+
+
+
 
 # Label and combobox where users select the day they want to enter their data
-Label(Tracker, text='Day :', width=25, height=2, font=20).pack(pady=2, padx=3)
+Label(Tracker, text='Day : (eg. Thursday)', width=25, height=2, font=20).pack(pady=2, padx=3)
 day_choice = StringVar()
-Day_frame = ttk.Combobox(Tracker, textvariable=day_choice, width=25, height=8)
-Day_frame['state'] = 'readonly'
-Day_frame['values'] = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+Day_frame = ttk.Entry(Tracker, textvariable=day_choice, width=25)
 Day_frame.pack(pady=2, padx=3)
 
 
-# Label and combobox where users select how many hours they've done of physical activity
-Label(Tracker, text='Hours and Minutes done :', width=25, height=2, font=20, ).pack(pady=2, padx=3)
+
+# Label and combobox where users enter how much time they've done of physical activity
+Label(Tracker, text='Time done : (eg. 95)', width=25, height=1, font=10).pack(pady=2, padx=3)
 hour_choice = StringVar()
-Hour_frame = ttk.Combobox(Tracker, textvariable=hour_choice, width=25, height=8)
-Hour_frame['state'] = 'readonly'
-Hour_frame['values'] = ('0 hr', '1 hr', '2 hr', '3 hr', '4 hr', '5 hr')
+Hour_frame = ttk.Entry(Tracker, textvariable=hour_choice, width=25)
 Hour_frame.pack(pady=2, padx=3)
 
-# Combobox where users select how many minutes they've done of physical activity
-minute_choice = StringVar()
-Minute_frame = ttk.Combobox(Tracker, textvariable=minute_choice, width=25, height=8)
-Minute_frame['state'] = 'readonly'
-Minute_frame['values'] = ('5 minutes', '10 minutes', '15 minutes', '20 minutes', '25 minutes', '30 minutes', '35 minutes',
-                          '40 minutes', '45 minutes', '50 minutes', '55 minutes')
-Minute_frame.pack(pady=20, padx=3)
+
+# Window that pops up with calculations
+def trackerMessage():
+   day = Day_frame.get()
+   time = Hour_frame.get()
+   messagebox.showinfo(print('Day: {}', 'Time: {}'.format(day, time)))
 
 
-def getSelection(Day_frame, Hour_frame, Minute_frame):
-    val = Day_frame.get(), Hour_frame.get(), Minute_frame.get()
-    print.show("Selection", val)
+def print(Tracker):
+    print(Day_frame.get())
+    print(Hour_frame.get())
 
 
 # Button for user to enter their data chosen
-Enter_button = ttk.Button(Tracker, text='Enter', width=10,
-                          command= lambda: getSelection(Day_frame, Hour_frame, Minute_frame)).pack(pady=5)
+Enter_button = ttk.Button(Tracker, text='Enter', width=10, command=trackerMessage).pack(pady=5)
+
 
 # button that allows users to go back to the home page
 Button(Tracker, text='Home', width=15, height=1, font=50, bg='lightblue', command=lambda: raise_frame(Home)).pack(
@@ -72,6 +76,8 @@ Button(Tracker, text='Home', width=15, height=1, font=50, bg='lightblue', comman
 # creating the labels and buttons that will be shown on the help page
 Label(Help, text='Help', width=90, height=5, font=80).pack()
 Label(Help, text='Welcome to the Help Page', width=50, height=3, font=50).pack()
+
+
 Button(Help, text='Home', width=30, height=2, font=50, bg='lightblue', command=lambda: raise_frame(Home)).pack()
 
 raise_frame(Home)
